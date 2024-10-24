166cc4619ec91fdcc99cdb1e4884ea5486bd0902
Sebastian Román Sánchez <sebastian.romans@uqvirtual.edu.co>
2024-10-24 15:58:20 -0500

    Suma
    Se añade lo necesario para realizar la operación de suma.

--- calculadora.asm
+++ calculadora.asm
@@ -43,7 +43,7 @@
         mov ah,9
         int 21h
 
-        lea dx,msg_Resta 
+        lea dx,msg_Resta
         mov ah,9
         int 21h
 
@@ -51,15 +51,15 @@
         mov ah,9
         int 21h
 
-        lea dx,msg_Op_Division 
+        lea dx,msg_Op_Division
         mov ah,9
         int 21h
 
-        lea dx,msg_Salir 
+        lea dx,msg_Salir
         mov ah,9
         int 21h
 
-        lea dx,msg_Opcion 
+        lea dx,msg_Opcion
         mov ah,9
         int 21h
 
@@ -84,3 +84,43 @@
 
     cmp bh,5
     je exit_p
+
+;SUMA
+    Op_Suma:
+    lea dx, msg_PrimerNumero
+    mov ah,9
+    int 21h
+
+    mov ah,1
+    int 21h
+    mov bl,al
+
+    lea dx,msg_SegundoNumero
+    mov ah,9
+    int 21h
+
+    mov ah,1
+    int 21h
+    mov cl,al
+
+    add al,bl
+    mov ah,0
+    aaa
+
+    mov bx,ax
+    add bh,48
+    add bl,48
+
+    lea dx,msg_Resultado
+    mov ah,9
+    int 21h
+
+    mov ah,2
+    mov dl,bh
+    int 21h
+
+    mov ah,2
+    mov dl,bl
+    int 21h
+
+    jmp menu
